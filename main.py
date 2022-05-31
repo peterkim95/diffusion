2998c18b1d511fe1b4ea191848303f92b7833673
Peter Kim <peter.kim@buzzvil.com>
2022-05-31 14:35:32 +0000

    train longer

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ trainer = Trainer(
     gradient_accumulate_every=2,  # gradient accumulation steps
     ema_decay=0.995,  # exponential moving average decay
     amp=True,  # turn on mixed precision
-    save_and_sample_every=100
+    save_and_sample_every=1000
 )
 
 trainer.train()
